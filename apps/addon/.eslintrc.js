8834ce162c3165f41162c0628a1dd41f21c94d6f
Davi Alexandre <deltreertl@gmail.com>
2024-08-27 22:48:39 -0300

    chore: use workspaces instead of turborepo

--- apps/addon/.eslintrc.js
+++ apps/addon/.eslintrc.js
@@ -1,4 +1,27 @@
+const { resolve } = require('node:path');
+
+const project = resolve(process.cwd(), 'tsconfig.json');
+
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ['@repo/eslint-config/base.js'],
+  extends: ['eslint:recommended', 'prettier', 'turbo'],
+  plugins: ['only-warn'],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: [
+    // Ignore dotfiles
+    '.*.js',
+    'node_modules/',
+    'dist/',
+  ],
+  overrides: [
+    {
+      files: ['*.js?(x)', '*.ts?(x)'],
+    },
+  ],
 };
